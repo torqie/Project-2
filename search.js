033dbaf9253b12d8f30bfeb2e8e5da1bce6bcc2d
jonmeidell <jonmeidell@gmail.com>
2020-06-11 21:05:47 -0600

    added api element

--- search.js
+++ search.js
@@ -2,8 +2,18 @@ const searchBar = new searchingID();
 searchBar.readyStateChange = function (options) {
     if (this.readyState == 4 && this.status == 200) {
         // look-up what categoryID is called
-        document.getElementById("categoryID").innerHTML = searchBar.ressponseText;
+        document.getElementById("categoryId").innerHTML = searchBar.ressponseText;
     };
 
     const limit = options.limit || 12;
 };
+
+const settings = {
+    "url": "/api/search?categoryId=&q=Tutorial 2",
+    "method": "GET",
+    "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+});
