d6dbb979c7e48ee3ddef0c4390b34d6cf5c37e61
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-06-11 20:03:09 -0600

    created ajax function for updating content

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -7,3 +7,16 @@ var create = {
 $.ajax(create).done(function(response) {
     console.log(response);
 });
+
+var update = {
+    "url": "/api/categories",
+    "method": "POST",
+    "timeout": 0,
+    "data": {
+        "name": "/"
+    }
+};
+
+$.ajax(update).done(function(response) {
+    console.log(response);
+});
