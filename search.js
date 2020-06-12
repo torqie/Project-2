67ffb3e9708dc2da20404480970e45653b523a93
jonmeidell <jonmeidell@gmail.com>
2020-06-11 20:57:43 -0600

    limit options to 12

--- search.js
+++ search.js
@@ -1,7 +1,9 @@
 const searchBar = new searchingID();
-searchBar.readyStateChange = function() {
+searchBar.readyStateChange = function (options) {
     if (this.readyState == 4 && this.status == 200) {
         // look-up what categoryID is called
         document.getElementById("categoryID").innerHTML = searchBar.ressponseText;
-    }
+    };
+
+    const limit = options.limit || 12;
 };
