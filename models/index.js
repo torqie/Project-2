571ed7b2b64713d5c11e096abc4a5f4cd051cc16
jonmeidell <jonmeidell@gmail.com>
2020-06-15 20:53:15 -0600

    added other minor effects

--- models/index.js
+++ models/index.js
@@ -8,9 +8,9 @@ const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
 if (config.use_env_variable) {
-  let sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  let sequelize = new Sequelize(
+  var sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
